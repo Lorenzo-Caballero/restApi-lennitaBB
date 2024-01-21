0697ad29fb3e090bad7178eeacdbb234058125ba
Lorenzo Caballero <zeldiuss@gmail.com>
2024-01-21 03:18:18 -0300

    added cors

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 import express from "express";
 import usersRoutes from "./routes/users.routes.js"
 import indexRoutes from "./routes/index.routes.js"
-
+import cors from "cors"
 const app = express();
 
 app.use(express.json())
 
 app.use(indexRoutes)
 app.use("/api",usersRoutes)
-import cors from "cors"
+
 
 app.use(cors({
     origin: 'http://localhost:3000', // Permite solicitudes desde este dominio
