71bdab58ef69f172172626da7e4444b464b819a6
Lorenzo Caballero <zeldiuss@gmail.com>
2024-01-21 02:48:18 -0300

    added cors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import app from "./app.js"
 import {PORT} from "./config.js"
+const cors = require('cors');
+
+// Configurar CORS
+app.use(cors({
+    origin: 'http://localhost:3000', // Permite solicitudes desde este dominio
+    methods: 'POST', // Permite solicitudes POST
+    allowedHeaders: 'Content-Type', // Permite el encabezado Content-Type
+  }));
 app.listen( PORT);
 console.log("server esta corriendo en puerto ",PORT)
