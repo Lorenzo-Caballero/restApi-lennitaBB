f500ac64dd89ec3c3b03363f3f75e3459194f082
Lorenzo Caballero <zeldiuss@gmail.com>
2024-01-21 03:15:47 -0300

    added cors

--- src/app.js
+++ src/app.js
@@ -8,6 +8,13 @@ app.use(express.json())
 
 app.use(indexRoutes)
 app.use("/api",usersRoutes)
+import cors from "cors"
+
+app.use(cors({
+    origin: 'http://localhost:3000', // Permite solicitudes desde este dominio
+    methods: 'POST', // Permite solicitudes POST
+    allowedHeaders: 'Content-Type', // Permite el encabezado Content-Type
+  }));
 app.use((req,res,next)=>{
     res.status(404).json({
         message:" faunotattoo: endpoint no encontrado:( Revisa la URL"
--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import app from "./app.js"
 import {PORT} from "./config.js"
-import cors from "cors"
 
-app.use(cors({
-    origin: 'http://localhost:3000', // Permite solicitudes desde este dominio
-    methods: 'POST', // Permite solicitudes POST
-    allowedHeaders: 'Content-Type', // Permite el encabezado Content-Type
-  }));
 app.listen( PORT);
 console.log("server esta corriendo en puerto ",PORT)
