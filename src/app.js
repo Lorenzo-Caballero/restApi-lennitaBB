bc55a9672b7f9b7aaed452cd1cfd175aa1008760
Lorenzo Caballero <zeldiuss@gmail.com>
2024-01-22 00:22:55 -0300

    added cors

--- src/app.js
+++ src/app.js
@@ -4,17 +4,19 @@ import indexRoutes from "./routes/index.routes.js"
 import cors from "cors"
 const app = express();
 
+app.use((req,res,next)=>{
+    res.append("Access-Control-Allow-Origin",["*"]);
+    res.append("Access-Control-Allow-Origin","GET,PUT,POST,DELETE");
+    res.append("Access-Control-Allow-Origin","Content-Type");
+    next()
+});
+
 app.use(express.json())
 
 app.use(indexRoutes)
 app.use("/api",usersRoutes)
 
 
-app.use(cors({
-    origin: 'http://localhost:3000', // Permite solicitudes desde este dominio
-    methods: 'POST', // Permite solicitudes POST
-    allowedHeaders: 'Content-Type', // Permite el encabezado Content-Type
-  }));
 app.use((req,res,next)=>{
     res.status(404).json({
         message:" faunotattoo: endpoint no encontrado:( Revisa la URL"
