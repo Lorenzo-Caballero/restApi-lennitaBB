39aa0831e615aac0d7194d246ab46cf8513f789d
Lorenzo Caballero <zeldiuss@gmail.com>
2024-02-05 01:08:26 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -1,25 +1,28 @@
 import express from "express";
-import usersRoutes from "./routes/users.routes.js";
-import designsRoutes from "./routes/designs.routes.js";
-import indexRoutes from "./routes/index.routes.js";
-import cors from "cors";
+import usersRoutes from "./routes/users.routes.js"
+import designsRoutes from "./routes/designs.routes.js"
+import indexRoutes from "./routes/index.routes.js"
 
 const app = express();
 
-// Configuración de CORS
-app.use(cors());
-
-app.use(express.json());
-
-app.use(indexRoutes);
-app.use("/api", usersRoutes);
-app.use("/api", designsRoutes);
-
-// Middleware para manejar rutas no encontradas
+// Middleware para habilitar CORS
 app.use((req, res, next) => {
-    res.status(404).json({
-        message: "faunotattoo: endpoint no encontrado:( Revisa la URL"
-    });
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
 });
+
+app.use(express.json())
+
+app.use(indexRoutes)
+app.use("/api",usersRoutes)
+app.use("/api",designsRoutes)
+
+app.use((req,res,next)=>{
+    res.status(404).json({
+        message:"faunotattoo: endpoint no encontrado:( Revisa la URL"
+    })
+})
 
 export default app;
