6a0929cefe6571747894c9794702bc21b7997e57
Lorenzo Caballero <zeldiuss@gmail.com>
2024-02-05 00:59:42 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -1,27 +1,25 @@
 import express from "express";
-import usersRoutes from "./routes/users.routes.js"
-import designsRoutes from "./routes/designs.routes.js"
-import indexRoutes from "./routes/index.routes.js"
-import cors from "cors"
+import usersRoutes from "./routes/users.routes.js";
+import designsRoutes from "./routes/designs.routes.js";
+import indexRoutes from "./routes/index.routes.js";
+import cors from "cors";
+
 const app = express();
+
+// Configuración de CORS
 app.use(cors());
 
+app.use(express.json());
+
+app.use(indexRoutes);
+app.use("/api", usersRoutes);
+app.use("/api", designsRoutes);
+
+// Middleware para manejar rutas no encontradas
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    next();
-});
-app.use(express.json())
-
-app.use(indexRoutes)
-app.use("/api",usersRoutes)
-app.use("/api",designsRoutes)
-
-app.use((req,res,next)=>{
     res.status(404).json({
-        message:" faunotattoo: endpoint no encontrado:( Revisa la URL"
-    })
-})
+        message: "faunotattoo: endpoint no encontrado:( Revisa la URL"
+    });
+});
 
 export default app;
