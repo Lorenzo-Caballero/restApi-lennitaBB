1b4a4c037ee72d23b8bb79c97a9863c70f292a99
Lorenzo Caballero <zeldiuss@gmail.com>
2024-02-05 01:23:03 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -3,17 +3,15 @@ import usersRoutes from "./routes/users.routes.js"
 import designsRoutes from "./routes/designs.routes.js"
 import indexRoutes from "./routes/index.routes.js"
 import cors from "cors"
-const bodyParser = require('body-parser');
-
 const app = express();
-
-// Middleware para habilitar CORS
 app.use(cors());
 
-// Middleware para permitir solicitudes preflight OPTIONS
-app.options('*', cors());
-app.use(bodyParser.text({ type: 'application/json' })); // to support JSON-encoded bodies (express-validator)
-
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+});
 app.use(express.json())
 
 app.use(indexRoutes)
@@ -22,7 +20,7 @@ app.use("/api",designsRoutes)
 
 app.use((req,res,next)=>{
     res.status(404).json({
-        message:"faunotattoo: endpoint no encontrado:( Revisa la URL"
+        message:" faunotattoo: endpoint no encontrado:( Revisa la URL"
     })
 })
 
