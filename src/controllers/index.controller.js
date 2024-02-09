12960b5c9c221d3995c18f62fec6bfa15f9588b3
Lorenzo Caballero <zeldiuss@gmail.com>
2024-02-09 03:31:42 -0300

    ai

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -3,6 +3,6 @@ import {config} from "dotenv"
 
 config()
 export const ping =async (req, res) => {
-  API_KEY= process.env.API-KEY-AI;
+   const API_KEY= process.env.API-KEY-AI;
   return  res.json(API_KEY)
 }
