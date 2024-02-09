452d12d5fe5e12c4ee6df62001da0ab3d71159d1
Lorenzo Caballero <zeldiuss@gmail.com>
2024-02-09 12:16:24 -0300

    pass

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,17 +1,9 @@
 import { pool } from "../db.js";
-import {config} from "dotenv"
-
-config()
-
-import { pool } from "../db.js";
-
 
 export const ping = async (req, res) => {
   try {
-    const rows = await pool.query('SELECT * FROM api');
+    const [rows] = await pool.query('SELECT * FROM api');
     res.json(rows);
-    client.release();
-    return res.json(rows);
   } catch (error) {
     console.error('Error al obtener la clave de la tabla "api":', error);
     return res.status(500).json({ error: 'Error interno del servidor' });
