22d281c99cb6a5e28eb56fe5835ca93e89813ce1
Lorenzo Caballero <zeldiuss@gmail.com>
2024-02-11 03:40:24 -0300

    table modify

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import usersRoutes from "./routes/users.routes.js";
 import designsRoutes from "./routes/designs.routes.js";
 import indexRoutes from "./routes/index.routes.js";
 import cors from "cors";
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -10,6 +11,8 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 app.use("/api",indexRoutes);
 app.use("/api", usersRoutes);
