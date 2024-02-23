170cb95cbec3e3b9191667e9b33bb973eb71d32f
Lorenzo Caballero <zeldiuss@gmail.com>
2024-02-22 23:26:34 -0300

    netlifly prod

--- src/functions/app.js
+++ src/functions/app.js
@@ -15,9 +15,9 @@ app.use(express.json());
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
-app.use("/.netlify/functions/app/api", indexRoutes);
-app.use("/.netlify/functions/app/api", usersRoutes);
-app.use("/.netlify/functions/app/api", designsRoutes);
+app.use("/.netlify/functions/app", indexRoutes);
+app.use("/.netlify/functions/app", usersRoutes);
+app.use("/.netlify/functions/app", designsRoutes);
 
 app.use((req, res, next) => {
     res.status(404).json({
