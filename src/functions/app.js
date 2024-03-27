e0f158c419d85f563ac2cc2c6b2a192cc610181e
Lorenzo Caballero <zeldiuss@gmail.com>
2024-03-27 20:29:14 -0300

    routes fixed

--- src/functions/app.js
+++ src/functions/app.js
@@ -1,28 +1,27 @@
 import express from "express";
-import usersRoutes from "../routes/users.routes.js";
-import designsRoutes from "../routes/designs.routes.js";
-import indexRoutes from "../routes/index.routes.js";
+import usersRoutes from "./routes/users.routes.js";
+import designsRoutes from "./routes/designs.routes.js";
+import indexRoutes from "./routes/index.routes.js";
 import cors from "cors";
-import serverless from "serverless-http";
 import bodyParser from 'body-parser';
 
 const app = express();
 
-// Middleware para habilitar CORS permitiendo solicitudes desde "cualquier origen
+// Middleware para habilitar CORS permitiendo solicitudes desde cualquier origen
 app.use(cors());
 
 app.use(express.json());
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
-app.use("/.netlify/functions/app", indexRoutes);
-app.use("/.netlify/functions/app", usersRoutes);
-app.use("/.netlify/functions/app", designsRoutes);
+app.use("/api",indexRoutes);
+app.use("/api", usersRoutes);
+app.use("/api", designsRoutes);
 
 app.use((req, res, next) => {
     res.status(404).json({
         message: "faunotattoo: endpoint no encontrado:( Revisa la URL"
     });
 });
-export const handler = serverless(app);
 
+export default app;
