1c34253e123cfa157c36eb1dcced5e5f4b7dee3c
Lorenzo Caballero <zeldiuss@gmail.com>
2024-03-31 01:53:21 -0300

    cors error arreglado

--- src/functions/app.js
+++ src/functions/app.js
@@ -1,7 +1,7 @@
 import express from "express";
-import usersRoutes from "../routes/users.routes.js";
-import designsRoutes from "../routes/designs.routes.js";
-import indexRoutes from "../routes/index.routes.js";
+import usersRoutes from "./routes/users.routes.js";
+import designsRoutes from "./routes/designs.routes.js";
+import indexRoutes from "./routes/index.routes.js";
 import cors from "cors";
 import bodyParser from 'body-parser';
 
@@ -14,14 +14,13 @@ app.use(express.json());
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
-app.use("/api",indexRoutes);
-app.use("/api", usersRoutes);
-app.use("/api", designsRoutes);
+app.use("/.netlify/functions/app", indexRoutes);
+app.use("/.netlify/functions/app", usersRoutes);
+app.use("/.netlify/functions/app", designsRoutes);
 
 app.use((req, res, next) => {
     res.status(404).json({
         message: "faunotattoo: endpoint no encontrado:( Revisa la URL"
     });
 });
-
 export default app;
