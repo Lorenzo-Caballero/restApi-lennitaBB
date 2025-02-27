03a54e2570f01dbe32d8d5307ea8ad4509d2f9e4
Lorenzo Caballero <zeldiuss@gmail.com>
2025-02-27 02:11:26 -0300

    api

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -3,7 +3,7 @@ import { pool } from "../db.js";
 export const ping = async (req, res) => {
   try {
     const [rows] = await pool.query('SELECT * FROM api');
-    res.json(rows[0].KEY_AI);
+    res.json(rows[0].ai);
   } catch (error) {
     console.error('Error al obtener la clave de la tabla "api":', error);
     return res.status(500).json({ error: 'Error interno del servidor' });
