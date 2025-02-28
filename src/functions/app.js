d10657d2169e9a3d6bf7792f49a7b88b0ffb7345
Lorenzo Caballero <zeldiuss@gmail.com>
2025-02-27 22:56:55 -0300

    routes transactions in app.js

--- src/functions/app.js
+++ src/functions/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import usersRoutes from "../routes/users.routes.js";
 import designsRoutes from "../routes/designs.routes.js";
+import transactionsRoutes from "../routes/transactions.routes.js"
 import indexRoutes from "../routes/index.routes.js";
 import cors from "cors";
 import bodyParser from 'body-parser';
@@ -16,7 +17,8 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use("/api", indexRoutes);
 app.use("/api", usersRoutes);
 app.use("/api", designsRoutes);
-
+app.use("/api", designsRoutes);
+app.use("/api",transactionsRoutes);
 app.use((req, res, next) => {
     res.status(404).json({
         message: "faunotattoo: endpoint no encontrado:( Revisa la URL"
