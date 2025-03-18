a3c769ca6f268a4acea0349fb96acf907bb7b653
Lorenzo Caballero <zeldiuss@gmail.com>
2025-03-17 21:35:25 -0300

    middleware of image

--- src/routes/designs.routes.js
+++ src/routes/designs.routes.js
@@ -2,14 +2,12 @@ import { Router } from "express";
 import { getDesigns,uploadMiddleware, createDesigns, updateDesigns, deleteDesigns, getDesignsById } from "../controllers/designs.controllers.js";
 const router = Router()
 
-app.post("/api/designs", uploadMiddleware, createDesigns);
-
 router.get("/designs", getDesigns)
 
 router.get("/designs/:id", getDesignsById)
 
 
-router.post("/designs", createDesigns)
+router.post("/designs", createDesigns,uploadMiddleware)
 
 router.put("/designs/:id", updateDesigns)
 
